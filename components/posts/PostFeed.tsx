2d687d3b1075bd1f903c48733284b61f4c759415
santidotpy <santidotpy@gmail.com>
2023-07-29 03:40:43 -0300

    🔒️ login to display posts

--- components/posts/PostFeed.tsx
+++ components/posts/PostFeed.tsx
@@ -1,6 +1,7 @@
-import usePosts from '@/hooks/usePosts';
+import usePosts from "@/hooks/usePosts";
 
-import PostItem from './PostItem';
+import PostItem from "./PostItem";
+import useCurrentUser from "@/hooks/useCurrentUser";
 
 interface PostFeedProps {
   userId?: string;
@@ -8,12 +9,22 @@ interface PostFeedProps {
 
 const PostFeed: React.FC<PostFeedProps> = ({ userId }) => {
   const { data: posts = [] } = usePosts(userId);
+  const { data: currentUser } = useCurrentUser();
 
   return (
     <>
-      {posts.map((post: Record<string, any>,) => (
-        <PostItem userId={userId} key={post.id} data={post} />
-      ))}
+      {!currentUser ? (
+        <div className="flex flex-col items-center text-white mt-5">
+          <h2 className="text-2xl font-bold text-white-500">Welcome to UMX!</h2>
+          <p>Please sign in to start browsing.</p>
+        </div>
+      ) : (
+        <div>
+          {posts.map((post: Record<string, any>) => (
+            <PostItem userId={userId} key={post.id} data={post} />
+          ))}
+        </div>
+      )}
     </>
   );
 };
